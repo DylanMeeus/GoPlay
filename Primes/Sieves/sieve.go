33d9e9cc5913b7b821f246eace7d73d1dc998162
Dylan Meeus <meeusdylan@hotmail.com>
2017-10-01 01:27:26 +0200

    Complete sieve

--- Primes/Sieves/sieve.go
+++ Primes/Sieves/sieve.go
@@ -2,13 +2,41 @@ package Primes
 
 
 import (
-    "fmt"
+    "math"
 )
 
-func Sieve(n int){
-    fmt.Println("Starting sieve")
+func Sieve(n int) []int{
+    revisedLimit := n + 2
+    a := make([]bool, revisedLimit)
+    setTrue(&a)
 
-    a := [n]bool{}
+    // the first 2 are false as 0, 1 != prime
+    a[0] = false
+    a[1] = false
 
-    fmt.Println(a)
+    limit := math.Sqrt(float64(revisedLimit))
+    for i := 2; float64(i) < limit; i++{
+        if a[i] {
+            k := 0
+            for j := i*i;j < revisedLimit; j = (i*i) + (k * i){
+                a[j] = false
+                k++
+            }
+        }
+    }
+
+    output := []int{}
+    for i := 0; i < len(a); i++{
+        if a[i]{
+            output = append(output,i)
+        }
+    }
+    return output
+}
+
+
+func setTrue(arr *[]bool){
+    for i:= 0; i < len(*arr); i++{
+        (*arr)[i] = true;
+    }
 }
